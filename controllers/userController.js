102bd957fbe1a44c68dc92ec00b7643ab416a506
Daniel Packer <danielapacker@gmail.com>
2022-11-28 23:08:40 -0700

    add userController get user funcs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,27 @@
-const { ObjectId } = require("mongoose").Types;
 const { User, Thought } = require("../models");
+
+module.exports = {
+  // get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => {
+        res.json(users);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  },
+  // get one user
+  getOneUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "user not found" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  },
+};
