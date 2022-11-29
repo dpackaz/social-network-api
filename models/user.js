cd45cb2648cbc43d78ee943260d4fa8307cc9d56
Daniel Packer <danielapacker@gmail.com>
2022-11-28 20:07:40 -0700

    add email validation regex for user.js

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+  },
+});
